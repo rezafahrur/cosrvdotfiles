54e5b142b89834a8af5585f12d7b0863831551a2
BeSt Admin <bestadm@BeStCoSrv-PSR>
2019-08-31 15:02:38 +0700

    fix script to run i3

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt update
 sudo apt install xorg mariadb-server alsamixergui network-manager-gnome i3 nitrogen unrar rar zip unzip wine32 wine64 compton rxvt clipit mousepad firefox-esr ranger rofi dunst lxappearance
 git clone https://github.com/rezafahrur/i3-gaps-deb.git
 cd $HOME/bin/i3-gaps-deb
-i3-gaps-deb
+./i3-gaps-deb
