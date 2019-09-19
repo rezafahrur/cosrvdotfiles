06ee766573ed018a10839b75543c0dad94453ee6
BeSt Admin <bestadm@BeStCoSrv>
2019-09-19 10:57:20 +0700

    add volumeicon-alsa

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install xorg mariadb-server alsamixergui network-manager-gnome i3 nitrogen unrar rar zip unzip wine32 wine64 compton rxvt clipit mousepad firefox-esr ranger rofi dunst lxappearance xvfb
+sudo apt install xorg mariadb-server alsamixergui network-manager-gnome i3 nitrogen unrar rar zip unzip wine32 wine64 compton rxvt clipit mousepad firefox-esr ranger rofi dunst lxappearance xvfb volumeicon-alsa
 git clone https://github.com/rezafahrur/i3-gaps-deb.git
 cd $HOME/bin/i3-gaps-deb
 ./i3-gaps-deb
