929a2d4e6e81de6a355df81e24847edc1dae36b5
BeSt Admin <bestadm@BeStCoSrv>
2019-09-19 10:43:20 +0700

    change copy.sh

--- copy.sh
+++ copy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo dpkg -i i3status_2.13-2_amd64.deb
+sudo cp i3status.conf /etc/i3status.conf
 mkdir -p $HOME/.config
 cp -R rofi $HOME/.config/rofi
 mkdir -p $HOME/.local
@@ -9,3 +11,10 @@ cp -R greeting $HOME/bin/greeting
 cp -R shutdown $HOME/bin/shutdown
 mkdir -p $HOME/.fonts
 cp Terminess\ \(TTF\)\ Nerd\ Font\ Complete\ Mono.ttf $HOME/.fonts/Terminess\ \(TTF\)\ Nerd\ Font\ Complete\ Mono.ttf
+cp -R .bashrc $HOME/.bashrc
+cp -R .aliases $HOME/.aliases
+cp -R .nanorc $HOME/.nanorc
+cp -R .Xdefaults $HOME/.Xdefaults
+cp -R .Xresources $HOME/.Xresources
+cp -R .xinitrc $HOME/.xinitrc
+cp -R startup.sh $HOME/.startup.sh
